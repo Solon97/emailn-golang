dd3718ae34ec18e932613f323218f6f6d2c29ece
Solon97 <solonb20@gmail.com>
2024-06-01 11:21:32 -0300

    refactor: refactor campaign service test

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -40,19 +40,23 @@ func Test_NewService(t *testing.T) {
 	assert.EqualError(err, internalerrors.ErrRepositoryNil.Error())
 }
 
-func Test_Create_NewCampaign(t *testing.T) {
+func Test_Create(t *testing.T) {
 	assert := assert.New(t)
 	repo := &repositoryMock{}
-	repo.On("Create", mock.Anything).Return(nil)
+	service, _ := NewCampaignService(repo)
+
+	t.Run("Success", func(t *testing.T) {
+		repo := &repositoryMock{}
+		repo.On("Create", mock.Anything).Return(nil)
 	service, _ := NewCampaignService(repo)
 
 	id, err := service.Create(newCampaign)
 
 	assert.NoError(err)
 	assert.NotEmpty(id)
-}
+	})
 
-func Test_Create_UseRepository(t *testing.T) {
+	t.Run("Use repository", func(t *testing.T) {
 	repo := &repositoryMock{}
 	repo.On("Create", mock.MatchedBy(func(c *Campaign) bool {
 		return c.Name == newCampaign.Name &&
@@ -64,26 +68,26 @@ func Test_Create_UseRepository(t *testing.T) {
 	service.Create(newCampaign)
 
 	repo.AssertExpectations(t)
-}
+	})
 
-func Test_Create_ValidationError(t *testing.T) {
-	assert := assert.New(t)
+	t.Run("Validation Error", func(t *testing.T) {
 	expectedError := fmt.Errorf(internalerrors.ErrMinFieldPattern, "name", "5")
-	repo := &repositoryMock{}
-	service, _ := NewCampaignService(repo)
 
 	_, err := service.Create(&dto.NewCampaign{})
 
 	assert.EqualError(err, expectedError.Error())
-}
+	})
 
-func Test_Create_RepositoryError(t *testing.T) {
-	assert := assert.New(t)
+	t.Run("Repository Error", func(t *testing.T) {
 	repo := &repositoryMock{}
 	repo.On("Create", mock.Anything).Return(errors.New("error"))
 	service, _ := NewCampaignService(repo)
 
 	_, err := service.Create(newCampaign)
+
 	assert.True(errors.Is(err, internalerrors.ErrInternalServer))
 	repo.AssertExpectations(t)
+	})
+}
+
 }
